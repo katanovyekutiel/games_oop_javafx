2366046129dfe0170d8f1b99b233d54c00a86802
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-10-10 14:33:15 +0300

    2. Каркас шахматной доски [#792 #214149]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.chess.firuges.black;
 
 import junit.framework.TestCase;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 
 public class BishopBlackTest extends TestCase {
 
@@ -8,5 +10,16 @@ public class BishopBlackTest extends TestCase {
         new BishopBlack(Cell.C8);
         exepted = Cell.C8;
         assertEquals(position();
+    }
+
+    public void testCopy() {
+        new BishopBlack(Cell.C8);
+        exepted = Cell.C8;
+        assertEquals(Figure.copy(equals(exepted)));
+    }
+
+    public void testWay() {
+        new BishopBlack(Cell.C1);
+
     }
 }
