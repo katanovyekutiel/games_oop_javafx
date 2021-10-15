d27459f909a5848508c5eb7564e9f681f271edc4
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-10-15 13:50:19 +0300

    2. Каркас шахматной доски [#792 #214149]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,8 +1,12 @@
 package ru.job4j.chess.firuges.black;
 
 import junit.framework.TestCase;
+import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class BishopBlackTest extends TestCase {
 
